d86cd1d26ae149c1d772fe1e35769d58598bd595
Aydan Hoover <hooveraydan@gmail.com>
2023-02-06 19:17:25 -0800

    Created repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatStr = "";
+    if (num <= 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        repeatStr += string;
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
