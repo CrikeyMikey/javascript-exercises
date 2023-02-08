df30aa6a68321fdc54ce88685610f078757d1790
Aydan Hoover <hooveraydan@gmail.com>
2023-02-07 20:31:29 -0800

    Completed removeFromArray, passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let toRemove = args.splice(1, args.length);
+    let result = args[0].filter(num => !toRemove.includes(num));
+    return result;
 };
 
 // Do not edit below this line
